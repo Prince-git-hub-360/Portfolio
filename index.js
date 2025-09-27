6be117ab699d6e006145debecdfa7c30e5c830bf
Prince Kumar <kumariafprince@gmail.com>
2025-09-27 20:35:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var options = {
-    strings: ["Frontend Developer", "Student", "Web Developer"], // Texts to type
+    strings: ["Student", "AI/ML Engineer", "Data Scientist", "Problem Solver"], // Texts to type
     typeSpeed: 100,      // Typing speed
     backSpeed: 25,       // Backspacing speed
     backDelay: 1000,     // Delay before backspacing
@@ -11,14 +11,12 @@ var options = {
 // Initialize Typed.js
 var typed = new Typed("#typed-output", options);
 
-
-
 function updateDateTime() {
   const now = new Date();
 
   // Format date
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  document.getElementById("live-date").textContent = now.toLocaleDateString("en-US", options);
+  const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  document.getElementById("live-date").textContent = now.toLocaleDateString("en-US", dateOptions);
 
   // Format time
   document.getElementById("live-time").textContent = now.toLocaleTimeString("en-US", { hour12: true });
@@ -29,4 +27,3 @@ setInterval(updateDateTime, 1000);
 
 // Run once at start
 updateDateTime();
-
